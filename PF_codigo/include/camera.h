a868c7f457509e07ea8ce5a976ffcbd161c8da1a
Karla Beatriz Rojas Chimal <112668597+KarlaRoChi@users.noreply.github.com>
2026-05-01 20:09:25 -0600

    Add files via upload

--- PF_codigo/include/camera.h
+++ PF_codigo/include/camera.h
@@ -12,16 +12,17 @@ enum Camera_Movement {
     FORWARD,
     BACKWARD,
     LEFT,
-    RIGHT
+    RIGHT,
+    UP,    // Movimiento vertical ascendente
+    DOWN   // Movimiento vertical descendente
 };
 
 // Default camera values
-const float YAW         = -90.0f;
-const float PITCH       =  0.0f;
-const float SPEED       =  10.0f;
-const float SENSITIVITY =  0.5f;
-const float ZOOM        =  45.0f;
-
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 10.0f;
+const float SENSITIVITY = 0.5f;
+const float ZOOM = 45.0f;
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
 class Camera
@@ -50,6 +51,7 @@ public:
         Pitch = pitch;
         updateCameraVectors();
     }
+
     // Constructor with scalar values
     Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
     {
@@ -84,6 +86,10 @@ public:
             Position -= Right * velocity;
         if (direction == RIGHT)
             Position += Right * velocity;
+        if (direction == UP)
+            Position += Up * velocity; // Alteración positiva sobre el eje vertical local
+        if (direction == DOWN)
+            Position -= Up * velocity; // Alteración negativa sobre el eje vertical local
     }
 
     // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
@@ -92,7 +98,7 @@ public:
         xoffset *= MouseSensitivity;
         yoffset *= MouseSensitivity;
 
-        Yaw   += xoffset;
+        Yaw += xoffset;
         Pitch += yoffset;
 
         // Make sure that when pitch is out of bounds, screen doesn't get flipped
@@ -129,9 +135,11 @@ private:
         front.y = sin(glm::radians(Pitch));
         front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
         Front = glm::normalize(front);
+
         // Also re-calculate the Right and Up vector
-        Right = glm::normalize(glm::cross(Front, WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-        Up    = glm::normalize(glm::cross(Right, Front));
+        // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+        Right = glm::normalize(glm::cross(Front, WorldUp));
+        Up = glm::normalize(glm::cross(Right, Front));
     }
 };
 #endif
